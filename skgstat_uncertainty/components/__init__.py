f2e9d4fd3dd4772b0b51639932ecbd1fe88b47fb
Mirko Mälicke <mirko@hydrocode.de>
2021-06-24 13:54:13 +0200

    added export_table component

--- skgstat_uncertainty/components/__init__.py
+++ skgstat_uncertainty/components/__init__.py
@@ -1,1 +1,2 @@
 from .project import project_management, variogram_selector, simulation_family_selector
+from .table import table_export_options
---
+++ skgstat_uncertainty/components/table.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+from string import ascii_letters
+from random import choice
+
+
+def table_export_options(df: pd.DataFrame, export_formats=['LaTeX', 'CSV', 'JSON', 'Markdown', 'HTML'], container=None):
+    # if no container set, create one
+    if container is None:
+        container = st
+    
+    # build the columns
+    if len(export_formats) > 1:
+        options, table_area  = container.beta_columns((1, 9))
+    elif len(export_formats) == 1:
+        options = table_area = container
+    else:
+        raise AttributeError('At least one export format needs to be set')
+    
+    # check options
+    if len(export_formats) == 1:
+        fmt = export_formats[0]
+    else:
+        fmt = options.radio('Format', options=export_formats, key=''.join([choice(ascii_letters) for _ in range(25)]))
+    
+    # handle export
+    if fmt.lower() == 'latex':
+        table_area.code(df.to_latex(index=None))
+    elif fmt.lower() == 'csv':
+        table_area.code(df.to_csv(index=None))
+    elif fmt.lower() == 'json':
+        table_area.code(df.to_json(orient='records', indent=4))
+    elif fmt.lower() == 'markdown':
+        table_area.code(df.to_markdown(None))
+    elif fmt.lower() == 'html':
+        table_area.code(df.to_html(index=None))
