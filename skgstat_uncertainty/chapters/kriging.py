02c4fee226f916283198a61b43e1b44d0138167a
Mirko Mälicke <mirko@hydrocode.de>
2021-11-04 08:22:08 +0100

    fix in kriging filed shape

--- skgstat_uncertainty/chapters/kriging.py
+++ skgstat_uncertainty/chapters/kriging.py
@@ -150,7 +150,8 @@ def apply_kriging(models: List[VarioModel], dataset: DataUpload, vario: VarioPar
     if opts['call_args'].get('ext_drift', False):
         _x, _y = np.array(opts['call_args']['ext_drift']).shape
     elif dataset.data.get('field_id', False):
-        parent_field = api.get_upload_data(id=dataset.data.get('field_id'))
+        parent_dataset = api.get_upload_data(id=dataset.data.get('field_id'))
+        parent_filed = parent_dataset.data['field']
         _x, _y = np.array(parent_field).shape
     else:
         c = vario.variogram.coordinates
