3a9fb8a8457bb3f0951fa88927e0b02100650a2f
Mirko Mälicke <mirko@hydrocode.de>
2021-11-08 08:04:21 +0100

    dataset plot import

--- skgstat_uncertainty/components/__init__.py
+++ skgstat_uncertainty/components/__init__.py
@@ -1,3 +1,3 @@
 from .data_selects import upload_handler, upload_auxiliary_data, data_selector
-from .plotting import single_result_plot
+from .plotting import single_result_plot, dataset_plot
 from .tables import model_table
