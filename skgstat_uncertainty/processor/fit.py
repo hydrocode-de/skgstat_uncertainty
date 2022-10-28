0aca892769f30510f0b4805948aa2e6231f3f08d
Mirko Mälicke <mirko@hydrocode.de>
2022-10-28 13:52:57 +0200

    fit processor docs update

--- skgstat_uncertainty/processor/fit.py
+++ skgstat_uncertainty/processor/fit.py
@@ -1,3 +1,34 @@
+"""
+Various metrics to assess the quality of a variogram model parameterization.
+These metrics are key to manually parameterizing variogram models in the context
+of uncertainty bounds.
+The metrics can be used solely or jointly to filter a number of parameterizations,
+to select the parameter candidates best represeting the uncertain variogram with
+respect to uncertainty bounds.
+
+The implemented metrics are:
+
+* RMSE: the root mean squared error adjusted to the confidence interval.
+* cv: leave-one-out cross-validation by using the parameters to predict an observation based on the sample using kriging
+* dic: DIC of a collection of parameterization associated to a specific model.
+* aic / bic: AIC / BIC of a collection of parameterizations associated to a specific model
+* empirical / structural risk of a parameterization with respect to all other parameter sets calculated for the same model type.
+
+.. warning::
+    We are not sure if the AIC / BIC calculation is scientifically valid if the optimal parameter set
+    may possibly not be included in the collection. Strictly speaking it applies only to the optimal 
+    parameter set.
+
+.. note::
+    The metrics dic / aic / bic are metrics on model type level.
+
+.. note:: 
+    The empirical / structural risk metrics are always calculated with respect to the whole
+    collection of parameters for the given model type. With each new parameterization in the
+    database (for the current confidence interval instance) the values for all other parameterizations
+    need to be updated.
+
+"""
 from typing import List, Union
 import numpy as np
 from numpy.random import choice
@@ -11,6 +42,27 @@ from skgstat_uncertainty.processor import rm
 
 
 def rmse(vario: VarioParams, interval: VarioConfInterval, params: dict) -> float:
+    """
+    Adjusted RMSE for the params with respect to the confidence interval of 
+    the passed empirical variogram. The RMSE is adjusted to yield 0 for bins
+    within the uncertainty bounds, even if there is a deviance to the 
+    experimental semi-variance values.
+
+    Parameters
+    ----------
+    vario : VarioParams
+        Empirical variogram estimation from the database.
+    interval : VarioConfInterval
+        Uncertainty bounds representation from the database.
+    params : dict
+        The variogram model parameterization to be assessed
+    
+    Returns
+    -------
+    rmse : float
+        RMSE value for params
+
+    """
     # build the variogram instance
     variogram = vario.variogram
 
@@ -44,6 +96,34 @@ def rmse(vario: VarioParams, interval: VarioConfInterval, params: dict) -> float
 
 
 def cv(vario: Union[VarioParams, Variogram], params: dict, k: int = 1, max_iter: int = None) -> float:
+    """
+    Leave-one-out cross-validation for the params for an estimated
+    empirical variogram. Each observation is interpolated using
+    ordinary kriging using all other observations. The RMSE of
+    all interpolations is returned.
+
+    Parameters
+    ----------
+    vario : VarioParams
+        Empirical variogram estimation from the database.
+    params : dict
+        The variogram model parameterization to be assessed
+    k : 1
+        Number of points to be omitted in each iteration.
+        Defaults to 1, if you change this number, it is not
+        a leave-one-out cv anymore.
+    max_iter : int
+        Maximum number of iterations. If None (default) the
+        length of observations is used. If smaller, the result
+        is not deterministic anymore.
+    
+    Returns
+    -------
+    rmse : float
+        RMSE value for all interpolated observations.
+
+    """
+
     # build the variogram instance
     variogram = rebuild_variogram(vario, params)
 
